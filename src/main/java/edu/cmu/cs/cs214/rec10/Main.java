de6445dd58600f1b9f5de116134421195462af76
FrozenFish141 <srm1120@163.com>
2021-04-07 15:57:37 -0400

    changes main

--- src/main/java/edu/cmu/cs/cs214/rec10/Main.java
+++ src/main/java/edu/cmu/cs/cs214/rec10/Main.java
@@ -13,6 +13,7 @@ import java.util.ServiceLoader;
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(Main::createAndStartFramework);
+        
     }
 
     private static void createAndStartFramework() {
